2f3157fbbad8ef4aa03d69bd54a56b44584c798b
ZwergB <ben@zwergb.de>
2018-06-04 17:21:46 +0200

    Added download support and removed loopstop You can now download the website and play the game offline as well (yeeahh) Also added lowering and raising of gamemusic while in the result screen

--- js/script.js
+++ js/script.js
@@ -9,8 +9,11 @@ const PIPE_SPEED = 4;
 const POINT_POS  = {x: 10, y: 30};
 const NORMAL_GRAV = 0.5;
 
-const AUDIO_ON  = 'media/Speaker_Icon_on.svg'
-const AUDIO_OFF = 'media/Speaker_Icon_off.svg'
+let githubLink = 'https://raw.githubusercontent.com/ZwergB/FlappyAlpaka/master/';
+
+let audioOn  = 'media/Speaker_Icon_on.svg'
+let audioOff = 'media/Speaker_Icon_off.svg'
+let AUDIO_VOLUME = 0.2;
 
 let bird;
 let pipes = [];
@@ -26,22 +29,31 @@ let stars = [];
 let idleAnimation = 0;
 let birdSprite;
 
-let sound;
+let bgMusic;
+let desiredVolume = 0;
+
+// if the website is executed locally, it will load all media from the github repo (becouse you can't use local resources with js)
+if (window.location.protocol.includes('http')) {
+    githubLink = '';
+}
 
 function preload() {
-    birdSprite = loadImage('media/alpaka.png');
-    sound = new Audio('media/gamesound.mp3');
-    sound.volume = 0.2;
+    audioOn  = githubLink + audioOn;
+    audioOff = githubLink + audioOff;
+    birdSprite = loadImage(githubLink + 'media/alpaka.png');
+    bgMusic = new Audio(githubLink + 'media/gamesound.mp3');
+    bgMusic.volume = AUDIO_VOLUME;
+    bgMusic.loop = true;
 }
 
 window.onload = function() {
     document.getElementById('sound').addEventListener('click', function() {
-        if (sound.paused) {
-            document.getElementById('sound').setAttribute('src', AUDIO_ON);
-            sound.play();
+        if (bgMusic.paused) {
+            document.getElementById('sound').setAttribute('src', audioOn);
+            bgMusic.play();
         } else {
-            document.getElementById('sound').setAttribute('src', AUDIO_OFF);
-            sound.pause();
+            document.getElementById('sound').setAttribute('src', audioOff);
+            bgMusic.pause();
         }
     })
 }
@@ -57,6 +69,7 @@ function setup() {
     }
 
     textSize(20);
+    frameRate(60);
     
     window.addEventListener('mousedown', function() {jump();});
     window.addEventListener('keydown', function(e) {
@@ -70,42 +83,54 @@ function setup() {
 function draw() {
     background(5);
 
-    drawBackgroundElements();
-
-    bird.update();
-    bird.show();  
-        
-    if (Math.random() < 0.05) {
-        stars.push(new Stars(width,Math.random() * height))
-    }
-
-    for (let i = pipes.length-1; i >= 0; i--) {
-        pipes[i].show();
-        pipes[i].update();
-        if (pipes[i].hit(bird)){
-            loose();
-            return;
-        }
-        
-        if ( !pipes[i].noHit && pipes[i].done(bird) ) {
-            pointPlus();
-        }
-
-        if (pipes[i].offscreen()) {
-            pipes.splice(i, 1);
-        }
-    }    
-
     if (gameState == 1) {
         if (cycleCount % 100 == 0) {
             pipes.push(new Pipe());
         }
+
+        stars.map(star => star.update());
+        bird.update();
+        
+        for (let i = pipes.length-1; i >= 0; i--) {
+            pipes[i].update();
+
+            if (pipes[i].hit(bird)){
+                loose();
+            }
+            
+            if ( !pipes[i].noHit && pipes[i].done(bird) ) {
+                pointPlus();
+            }
+    
+            if (pipes[i].offscreen()) {
+                pipes.splice(i, 1);
+            }
+        }    
+
+        addStars();
         
         cycleCount++;
     } else if (gameState == 0) {
         let noiseMove = noise(frameCount / 100) - 0.5;
         bird.y += noiseMove * 1.5;
+        addStars();
+        stars.map(star => star.update());
+     
+    } else if (gameState == 2) {
+        bgMusic.volume = lerp(bgMusic.volume, 0.05, 0.2);
     }
+    
+    bgMusic.volume = lerp(bgMusic.volume, desiredVolume, 0.1);
+
+    drawBackgroundElements();
+    bird.show();  
+    pipes.map(x => x.show());    
+}
+
+function addStars() {
+    if (Math.random() < 0.05) {
+        stars.push(new Stars(width, Math.random() * height))
+    }   
 }
 
 function jump() {
@@ -139,7 +164,6 @@ function drawBackgroundElements() {
     strokeWeight(5);
     for (let i = stars.length-1; i >= 0; i--) {
         stars[i].show();
-        stars[i].update();
 
         if (stars[i].x < -5) {
             stars.splice(i, 1);
@@ -161,8 +185,9 @@ function loose() {
     
     document.getElementById('points').innerHTML = points;
     
+    desiredVolume = 0.05;
     gameState = 2;
-    noLoop();
+     
 }
 
 function scoreRestart() {
@@ -182,7 +207,10 @@ function restart() {
     points = 0;
     cycleCount = 1;
     gravity = 0;
-    loop();
+
+    desiredVolume = AUDIO_VOLUME;
+    bgMusic.currentTime = 0;
+
 }
 
 function nextRainbowColor(currentColor = -1) {
@@ -207,7 +235,6 @@ class PointAnimation {
     update() {
         this.y--;
     }
-
 }
 
 // Stars
@@ -226,7 +253,6 @@ class Stars {
     update() {
         this.x -= PIPE_SPEED;
     }
-
 }
 
 // Pipe
@@ -243,7 +269,6 @@ class Pipe {
         function randomIntFromInterval(min,max){
             return Math.floor(Math.random()*(max-min+1)+min);
         }
-
     }
 
     show() {
@@ -301,21 +326,15 @@ class Bird {
 
     show() {
         fill(250);
+        if (gameState == 0 || gameState == 1) 
+            this.addToRainbowTail();
         drawRainbowTail(this.history);
         image(birdSprite, this.x-this.size/2, this.y-this.size/2, this.size, this.size);
-
-        // push();
-        // strokeWeight(3);
-        // for (let i = 0; i < this.history.length; i++) {
-        //     line(this.history[i].x, this.history[i].y-5, this.history[i].x, this.history[i].y+5) ;
-            
-        // }
-        // pop();
 
         function drawRainbowTail(history) {
             const topPoint = -20;
             const bottomPoint = 25;
-            const seperation = (abs(topPoint) + abs(bottomPoint)) / RAINBOW_PATTERN.length;
+            const seperation = (Math.abs(topPoint) + Math.abs(bottomPoint)) / RAINBOW_PATTERN.length;
             let currentSep = 0;
 
             push();
@@ -334,8 +353,21 @@ class Bird {
                 }
                 endShape();
             }   
-
             pop();
+        }
+    }
+
+    addToRainbowTail() {
+            
+        this.currentColor = (this.currentColor+1) % RAINBOW_PATTERN.length;
+        let historyEle = {x: this.x-10, y: this.y, color: RAINBOW_PATTERN[this.currentColor]};
+        this.history.push(historyEle)
+
+        if (this.history.length > this.maxHistory) 
+            this.history.splice(0, 1);
+
+        for (let i = 0; i < this.history.length; i++) {
+            this.history[i].x -= PIPE_SPEED; 
         }
     }
 
@@ -351,7 +383,7 @@ class Bird {
             this.velocity = 15;
 
         if (this.y+this.size/2 > height && this.velocity > 0.1)  {
-            this.velocity /= -2;
+            this.velocity /= -1.5;
             this.y = height-this.size/2;
             
         }   
@@ -360,15 +392,5 @@ class Bird {
             this.y = +this.size/2;
         }
 
-        this.currentColor = (this.currentColor+1) % RAINBOW_PATTERN.length;
-        let historyEle = {x: this.x-10, y: this.y, color: RAINBOW_PATTERN[this.currentColor]};
-        this.history.push(historyEle)
-
-        if (this.history.length > this.maxHistory) 
-            this.history.splice(0, 1);
-
-        for (let i = 0; i < this.history.length; i++) {
-            this.history[i].x -= PIPE_SPEED; 
-        }
     }
 }
